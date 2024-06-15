ff111e5d908de47c15255156b5928c5c8280ae93
Catalin Moldovan <moldovan.catalin@gmail.com>
2024-06-15 14:18:35 +0300

    WebGL _uploadDirtyTextures

--- src/platform/graphics/webgl/webgl-render-target.js
+++ src/platform/graphics/webgl/webgl-render-target.js
@@ -134,6 +134,7 @@ class WebglRenderTarget {
                         colorBuffer._width = Math.min(colorBuffer.width, device.maxRenderBufferSize);
                         colorBuffer._height = Math.min(colorBuffer.height, device.maxRenderBufferSize);
                         device.setTexture(colorBuffer, 0);
+                        colorBuffer.upload(true);
                     }
                     // Attach the color buffer
                     gl.framebufferTexture2D(
@@ -158,6 +159,7 @@ class WebglRenderTarget {
                     depthBuffer._width = Math.min(depthBuffer.width, device.maxRenderBufferSize);
                     depthBuffer._height = Math.min(depthBuffer.height, device.maxRenderBufferSize);
                     device.setTexture(depthBuffer, 0);
+                    depthBuffer.upload(true);
                 }
                 // Attach
                 if (target._stencil) {
